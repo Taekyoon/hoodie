50ce840f40a60e97d258812acd4333dce27ffb81
Taekyoon <tgchoi03@gmail.com>
2016-12-07 20:56:42 -0800

    feat: modified some statements to match origin stream

--- bin/setup.js
+++ bin/setup.js
@@ -15,15 +15,7 @@ if (saveRequested) {
   var packageJson = require(path.join(pathToAppRoot, 'package.json'))
 
   packageJson.scripts = packageJson.scripts || {}
-
-  if (packageJson.scripts[ 'start' ]) {
-    log.info('setup', 'start script already set to "' + packageJson.scripts[ 'start' ] +
-    ', you can start hoodie with "npm run start-hoodie" instead')
-
-    packageJson.scripts[ 'start-hoodie' ] = 'hoodie'
-  } else {
-    packageJson.scripts[ 'start' ] = 'hoodie'
-  }
+  packageJson.scripts[ 'start' ] = 'hoodie'
 
   var newPackageJson = JSON.stringify(packageJson, null, 2)
 
